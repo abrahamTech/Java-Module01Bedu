1e92fd866f7cbd940b423e9553b7b5e786b9dd7d
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-08-16 21:28:24 -0600

    feat(Session08Challenge01): add recursivity with File class

---
+++ Session08/Challenge01/myChallenge16082023/Challenge01.java
@@ -0,0 +1,37 @@
+package Session08.Challenge01.myChallenge16082023;
+
+import java.io.File;
+
+public class Challenge01 {
+    public static void main(String[] args) {
+        File initialDirectory = new File( "C:/Users/Abraham Gonz\u00E1lez/Documents/GitHub/ReposPubliGitHub/CursoJavaBedu/ModulesGitRepo/Java-Module01Bedu/Session08");
+
+        showDirectoryContent(initialDirectory);
+
+    }
+    
+    private static void placeSpaces(String fatherDirectory, char simbol){
+        System.out.print("[" + simbol + "]");
+
+        for (int i = 0; i < fatherDirectory.length() - 100; i++) {
+            System.out.print("-");
+        }
+    }
+
+    private static void showDirectoryContent(File directory) {
+        placeSpaces(directory.getParent(), 'D');
+        System.out.println(directory.getName());
+
+        File[] subdirectories = directory.listFiles();
+
+        for (File archivo : subdirectories) {
+            if (archivo.isFile()) {
+                placeSpaces(archivo.getParent(), 'A');
+                System.out.println(archivo.getName());
+            } else if (archivo.isDirectory()) {
+                showDirectoryContent(archivo);
+            }
+        }
+    }
+
+}
