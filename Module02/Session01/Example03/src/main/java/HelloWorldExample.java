83f6d0d2958250c0d9a4ff06b341348eb7d01da7
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-08-30 23:20:53 -0600

    feat(Module02Session01) add Example03 (add plugins and application in build.gradle file)

---
+++ Module02/Session01/Example03/src/main/java/HelloWorldExample.java
@@ -0,0 +1,8 @@
+public class HelloWorldExample {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java ");
+
+        //1.- Lists tasks with `gradle tasks` in the menu
+        //2.- Run with `gradle run` in the menu
+    }
+}
