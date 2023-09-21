acbb9b1622483eae16974ef3534a693d0f639318
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-09-20 20:45:50 -0600

    feat(Session07Challenge01): dynamically generate a message that is displayed on an HTML page generated with Thymeleaf

--- Module02/Session07/src/main/java/org/abraham/java/backend/Session07/controllers/ControllerGreeting.java
+++ Module02/Session07/src/main/java/org/abraham/java/backend/Session07/controllers/ControllerGreeting.java
@@ -2,6 +2,7 @@ package org.abraham.java.backend.Session07.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,6 +13,13 @@ public class ControllerGreeting {
         //ModelAndView is a class in Spring MVC that acts as a container for Model and View. Allows a controller to return both the model and the view in a single return value.
         ModelAndView modelAndView = new ModelAndView("hello"); //Needs to be the same NAME as the HTML file that we are going to return
         modelAndView.addObject("message", "Hi from thymeleaf");
+        return modelAndView;
+    }
+
+    @GetMapping("/helloParam")
+    public ModelAndView hello(@RequestParam(name = "message") String message) {
+        ModelAndView modelAndView = new ModelAndView("hello");
+        modelAndView.addObject("message", message);
         return modelAndView;
     }
 }
