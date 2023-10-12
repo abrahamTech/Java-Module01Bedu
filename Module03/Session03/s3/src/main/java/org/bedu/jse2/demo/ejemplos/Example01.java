c00ff3f03c2301d2fe75fcd771d02f631c343178
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-10-11 20:09:55 -0600

    feat(Session03Challenge01): use Java's own interfaces

--- Module03/Session03/s3/src/main/java/org/bedu/jse2/demo/ejemplos/Example01.java
+++ Module03/Session03/s3/src/main/java/org/bedu/jse2/demo/ejemplos/Example01.java
@@ -1,23 +1,33 @@
 package org.bedu.jse2.demo.ejemplos;
 
+import java.util.function.Function;
+
 public class Example01 {
 
     //With Anonymous function
-    private final StringToInteger parser = new StringToInteger() {
-        @Override
-        public Integer convert(String str) {
-            return null;
-        }
-    };
+    //private final StringToInteger parser = new StringToInteger() {
+    //    @Override
+    //    public Integer convert(String str) {
+    //        return null;
+    //    }
+    //};
 
     //With Lambda Function
     //private final StringToInteger parser = str -> Integer.parseInt(str);
 
     //Lambda with reference method
-    //private final StringToInteger parser = Integer::parseInt;
+    /*private final StringToInteger parser = Integer::parseInt;
 
     Integer add(String a, String b){
         return parser.convert(a) + parser.convert(b);
+    }*/
+
+    //Challenge01
+    private final Function<String, Integer> parser = Integer::parseInt;
+
+    Integer add(String a, String b){
+        return parser.apply(a) + parser.apply(b);
     }
+
 
 }
