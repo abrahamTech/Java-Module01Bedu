515a2f6495e32105d3ab2f63657e43b9db46a1b5
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-10-11 21:52:21 -0600

    feat(Session03Extra): make use of streams to check values ​​of a set and see the difference between iterating with FOR and with STREAM

--- Module03/Session03/s3/src/test/java/org/bedu/jse2/demo/ejemplos/Example03Test.java
+++ Module03/Session03/s3/src/test/java/org/bedu/jse2/demo/ejemplos/Example03Test.java
@@ -12,8 +12,6 @@ import java.util.stream.IntStream;
 
 class Example03Test {
 
-    IntStream iterator = new Random().ints(10, 1, 2);
-
     @Test
     @DisplayName("Manipulate and add")
     void transformsAndAdsUp(){
@@ -26,5 +24,43 @@ class Example03Test {
         assertThat(example03.manipulate(set, i -> i < 0 ? i:-i)).isEqualTo(expected);
 
     }
+
+    //Extra
+    IntStream iterator = new Random().ints(10, 1, 2);
+
+    @Test
+    @DisplayName("Add numbers")
+    void addNumbers() {
+        assertThat(iterator.sum()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("Check values")
+    void checkValues() {
+        System.out.println(Arrays.toString(iterator.toArray()));
+    }
+
+    List<String> list = Arrays.asList("Anthony", "Maria", "Jhon", "Peter", "Raphael", "Jim");
+
+    @Test
+    @DisplayName("Iterate with For")
+    void iterateWithFor(){
+        int i = 0;
+        for (String name : list) {
+            if (name.contains("a")) {
+                i++;
+            }
+        }
+        //A !== a That explains why it gets 2
+        assertThat(i).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("Iterate with Stream")
+    void iterateWithStream(){
+        //A !== a That explains why it gets 2
+        assertThat(list.stream().filter(name -> name.contains("a")).count()).isEqualTo(2);
+    }
+
 
 }
