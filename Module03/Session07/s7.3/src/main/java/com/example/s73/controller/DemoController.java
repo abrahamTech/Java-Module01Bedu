5913277958bd3cf225e783b80604e88b31306a87
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-10-25 21:16:16 -0600

    feat(Session07Challenge02): reaffirm knowledge of Spring webflux

--- Module03/Session07/s7.3/src/main/java/com/example/s73/controller/DemoController.java
+++ Module03/Session07/s7.3/src/main/java/com/example/s73/controller/DemoController.java
@@ -3,10 +3,9 @@ package com.example.s73.controller;
 import com.example.s73.entity.Worker;
 import com.example.s73.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/workers")
@@ -16,9 +15,21 @@ public class DemoController {
     @Autowired
     private WorkerRepository workerRepository;
 
-    
+
     @GetMapping
     private Flux<Worker> getAllWorkers() {
         return workerRepository.findAllWorkers();
+    }
+
+    @GetMapping("/{id}")
+    private Mono<Worker> getWorkerById(@PathVariable String id) {
+        return workerRepository.findWorkerById(id);
+    }
+
+    //@PostMapping("/update")
+    //@PutMapping("/update")
+    @PutMapping //Only with http://localhost:8080/workers works (Don't need other URL "/update")
+    private Mono<Worker> updateWorker(@RequestBody Worker worker) {
+        return workerRepository.updateWorker(worker);
     }
 }
