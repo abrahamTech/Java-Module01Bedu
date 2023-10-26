16d8fe6f711f0a409ef2604ecf4b3091577f8582
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-10-25 20:02:00 -0600

    feat(Session07Challenge01): reaffirm knowledge of Spring boot

--- Module03/Session07/s7.2/src/main/java/com/example/s72/controller/DemoController.java
+++ Module03/Session07/s7.2/src/main/java/com/example/s72/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.s72.controller;
 
 import com.example.s72.entity.Car;
+import com.example.s72.entity.House;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,5 +10,10 @@ public class DemoController {
     @PostMapping("/car")
     public  void receive(@RequestBody Car car) {
         System.out.println(car.toString());
+    }
+
+    @PostMapping("/house")
+    public void recibeCasa(@RequestBody House house){
+        System.out.println(house.toString());
     }
 }
---
+++ Module03/Session07/s7.2/src/main/java/com/example/s72/entity/House.java
@@ -0,0 +1,10 @@
+package com.example.s72.entity;
+
+import lombok.Data;
+
+@Data
+public class House {
+    private int id;
+    private String color;
+    private int floors;
+}
