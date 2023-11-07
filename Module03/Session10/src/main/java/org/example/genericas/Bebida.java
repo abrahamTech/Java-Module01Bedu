7dadba6df0019d72ba025df1294ef9e200839630
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-11-06 21:29:23 -0600

    feat(Session10Example03): add code for Generic Classes topic

---
+++ Module03/Session10/src/main/java/org/example/genericas/Bebida.java
@@ -0,0 +1,17 @@
+package org.example.genericas;
+
+public class Bebida {
+    String sabor;
+
+    public Bebida(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+}
