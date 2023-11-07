e67960ad4f29f222019342e3e4a7a108a21992e1
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-11-06 20:17:32 -0600

    feat(Session10Example02): add code for Lambda topic

--- Module03/Session10/src/main/java/org/example/Main.java
+++ Module03/Session10/src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 import org.example.hilos.Hilos;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -19,6 +26,47 @@ public class Main {
             Thread hilo = new Hilos("Hilo " + i, 10000);
             hilo.setDaemon(true);
             hilo.start();
+
+        }
+
+        //Example 02 - LAMBDA
+        System.out.println(sizeOf.apply("Bedu"));
+        System.out.println(sizeOfLambda.apply("Bedu"));
+        System.out.println(new SizeOfFunction().apply("Bedu"));
+
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona("Beto"));
+        personas.add(new Persona("Carlos"));
+        personas.add(new Persona("Andrés"));
+
+        personas.sort(Comparator.comparing(p -> p.nombre));
+
+        Stream<Persona> stream = personas.stream();
+        List<String> nombres = stream
+                .filter(p -> p.nombre != null)
+                .map(p -> p.nombre)
+                .collect(Collectors.toList());
+
+        System.out.println(nombres);
+    }
+
+    //Example 02 - LAMBDA
+    static Function<String, Integer> sizeOf = (String placeholder) -> {
+        return placeholder.length();
+    };
+
+    static Function<String, Integer> sizeOfLambda = placeholder -> placeholder.length();
+
+    public static class SizeOfFunction implements Function<String, Integer> {
+        public Integer apply(String placeholder){
+            return placeholder.length();
+        }
+    }
+
+    static class Persona{
+        String nombre;
+        Persona(String nombre){
+            this.nombre = nombre;
         }
     }
 }
