5c3d241cef1cd3e0ed4786a3c5bb730e5fc8f78a
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-11-08 21:43:40 -0600

    feat(Session01Example03): add code for a basic calculator

--- Module04/Session01/src/main/java/org/abrahamtech/Session01/Session01Application.java
+++ Module04/Session01/src/main/java/org/abrahamtech/Session01/Session01Application.java
@@ -2,6 +2,7 @@ package org.abrahamtech.Session01;
 
 import java.util.Scanner;
 
+import org.abrahamtech.Session01.util.Calculator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +15,7 @@ public class Session01Application implements CommandLineRunner{
 	}
 
 	//You can use "@Override" with the Interface "CommandLineRunner"
-	@Override
+	/*@Override
 	public void run(String... args){
 		Scanner sc = new Scanner(System.in);
 
@@ -31,6 +32,65 @@ public class Session01Application implements CommandLineRunner{
 		System.out.println("Result: " + result);
 		sc.close();
 		
+	}*/
+
+	@Override
+	public void run(String... args){
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("***********************");
+		System.out.println("*Select the operation:*");
+		System.out.println("***********************");
+		System.out.println("1.- Add");
+		System.out.println("2.- Substract");
+		System.out.println("3.- Multiply");
+		System.out.println("4.- Divide");
+		System.out.println("************************");
+
+		System.out.println(">>");
+
+		int option = sc.nextInt();
+		sc.nextLine();
+
+		if(option < 1 || option > 4) {
+			System.out.println("Don't exist the operation...");
+			return;
+		}
+
+		System.out.println("Enter the first number: ");
+		int a = sc.nextInt();
+		sc.nextLine();
+
+		System.out.println("Enter the second number: ");
+		int b = sc.nextInt();
+		sc.nextLine();
+
+		int result = 0;
+
+		Calculator calculator = new Calculator();
+
+		switch (option) {
+			case 1:
+				result = calculator.add(a, b);
+				break;
+
+			case 2:
+				result = calculator.subs(a, b);
+				break;
+
+			case 3:
+				result = calculator.mult(a, b);
+				break;
+
+			case 4:
+				result = calculator.div(a, b);
+				break;
+		
+			default:
+				break;
+		}
+
+		System.out.println("Result: " + result);
 	}
 
 }
---
+++ Module04/Session01/src/main/java/org/abrahamtech/Session01/util/Calculator.java
@@ -0,0 +1,20 @@
+package org.abrahamtech.Session01.util;
+
+public class Calculator {
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subs(int a, int b) {
+        return a - b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+}
